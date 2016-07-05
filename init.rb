fdf303511a408be30a8aa6da04a5f102313adb36
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-04 22:58:40 -0300

    Redmine plugin to help anothers Redmine plugins.

---
+++ init.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :redmine_plugins_helper do
+  name 'Redmine Plugins\' Helper'
+  author 'Eduardo Henrique Bogoni'
+  description 'Helper for Redmine plugins'
+  version '0.1.0'
+end

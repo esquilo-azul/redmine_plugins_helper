390c9cbf4a1b9e8d6acc74ddf54c0f2379adfd7a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-04-23 15:09:37 -0300

    Execute initializers only if settings table exists.

---
+++ lib/redmine_plugins_helper.rb
@@ -0,0 +1,7 @@
+module RedminePluginsHelper
+  class << self
+    def settings_table_exist?
+      ActiveRecord::Base.connection.table_exists? ::Setting.table_name
+    end
+  end
+end
--- lib/redmine_plugins_helper/settings.rb
+++ lib/redmine_plugins_helper/settings.rb
@@ -1,7 +1,10 @@
+require_dependency 'redmine_plugins_helper'
+
 module RedminePluginsHelper
   class Settings
     class << self
       def default(plugin, default)
+        return unless ::RedminePluginsHelper.settings_table_exist?
         p = ::Setting.send("plugin_#{plugin}")
         p ||= {}
         default.each do |k, v|

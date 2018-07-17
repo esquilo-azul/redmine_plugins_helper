3efe1c5b92d26d2895d0c9d0a57d190ff982e02e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-07-17 14:00:46 -0300

    Fix default settings.

--- lib/redmine_plugins_helper/settings.rb
+++ lib/redmine_plugins_helper/settings.rb
@@ -5,12 +5,20 @@ module RedminePluginsHelper
     class << self
       def default(plugin, default)
         return unless ::RedminePluginsHelper.settings_table_exist?
-        p = ::Setting.send("plugin_#{plugin}")
-        p ||= {}
+        p = plugin_current_setting_value(plugin)
         default.each do |k, v|
-          p[k] = v unless p.key?(k)
+          p[k.to_s] = v unless p.key?(k)
         end
         ::Setting.send("plugin_#{plugin}=", p)
+      end
+
+      private
+
+      def plugin_current_setting_value(plugin)
+        p = ::Setting.send("plugin_#{plugin}")
+        p = {} unless p.is_a?(::Hash)
+        p = p.with_indifferent_access
+        p
       end
     end
   end

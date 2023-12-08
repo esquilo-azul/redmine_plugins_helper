942c6830e6b29880401e973cdebbee52d87bf1c5
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2023-12-07 23:35:34 -0300

    .rubocop.yml#Style/RedundantRegexpEscape: remove.

--- lib/redmine_plugins_helper/migration/database.rb
+++ lib/redmine_plugins_helper/migration/database.rb
@@ -6,7 +6,7 @@ module RedminePluginsHelper
       common_concern
 
       DATABASE_CORE_VERSION_PARSER = /^(\d+)$/.to_parser { |m| [PLUGIN_ID_CORE_VALUE, m[1]] }
-      DATABASE_PLUGIN_VERSION_PARSER = /^(\d+)\-(\S+)$/.to_parser { |m| [m[2], m[1]] }
+      DATABASE_PLUGIN_VERSION_PARSER = /^(\d+)-(\S+)$/.to_parser { |m| [m[2], m[1]] }
       DATABASE_VERSION_PARSERS = [DATABASE_PLUGIN_VERSION_PARSER, DATABASE_CORE_VERSION_PARSER]
                                    .freeze
 
